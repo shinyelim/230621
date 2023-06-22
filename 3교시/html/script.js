144260864650f7d37c6b1a554c75311cee7eb770
shinyelim <cac567@naver.com>
2023-06-22 16:19:32 +0900

    📝 버튼 애니메이션 효과주기

--- 3교시/html/script.js
+++ 3교시/html/script.js
@@ -1,3 +1,4 @@
+const cat  = document.getElementById('cat')
 const pointer = document.getElementById('pointer')
 console.log(pointer)
 
@@ -7,7 +8,31 @@ console.log('마우스 X값 : ${event.clientX}, 마우스 Y값: ${event.clientY}
 pointer.style.top = `${Event.clientY - (pointer.offsetHeight / 2)}px`
 pointer.style.left = `${Event.clientX - (pointer.offsetHeight / 2)}px`
 })
-pointer. style
+
+//클릭할때마다 생성을 한다.//
+let posX;//underfiend라고 f12에 찍힘//
+let posY;
+//console.log(posY)
+
+window.addEventListener('click',(event) => {
+//createElement -> span 태그 생성 참고로 fixed를 줘야함
+//태그는 사용해도 무관함//
+  const blu = document.createElement('span')
+  posX = event.clientX
+  posY = event.clientY
+  blu.classList.add('blu')
+  blu.classList.add('active')
+  cat.appendChild(blu)
+
+ blu.style.top = posY - (blu.offsetHeight/2)+`px`
+ blu.style.left = posX - (blu.offsetWidth/2)+`px`
+
+ setTimeout(() => {
+  blu.remove()
+ }, 1000);
+  console.log(blu)
+//셋타이밍에서 전체요소를 삭제하고싶으면 전체에담고있는 클래스나아이디에.remonve()를 넣는다.
+})
 //F12에서 마우스좌표값을 자바스크립트파일에 쓴것처럼 함수와함께 볼 수 있다.//
 //커셔움직일때 주는 공식을  쓸때는 주로 백테(~/`)를 주로 쓴다.
 //window는 창전체 즉 화면전체를 쓰는것이다.
@@ -16,3 +41,5 @@ pointer. style
 //console.log(pointer.clientwidth)
 //event객체에서 사용자의 x값을 가져와서, pointer의 가로값(36px)을 반값으로 나눈
 //18px을 뺀다. EX)x값이100일경우, 100, 100 - 18, 82px, 커서는 위치가 82px
+//자바스크립트는 html에서 동적으로 많이 사용함
+//sass는 현재 css비해 간결하게 정리가 됨
